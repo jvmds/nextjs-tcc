bbdeccdd047d4a268fc225c62aa3bae72df90f5c
jvmds <104597673+jvmds@users.noreply.github.com>
2024-04-28 15:50:06 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,14 +16,6 @@ export default function Home() {
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
