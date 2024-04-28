4131d3ae4551e2b238d14995248ec179c2ad1b47
jvmds <104597673+jvmds@users.noreply.github.com>
2024-04-28 15:44:01 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,7 @@ export default function Home() {
       <main className={styles.main}>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
