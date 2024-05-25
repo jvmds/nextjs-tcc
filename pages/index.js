c50d5b72a9f57c9f576af336bd9fc8842f02305b
jvmds <104597673+jvmds@users.noreply.github.com>
2024-05-25 18:28:33 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>Vai dá certo? não sei 2</h1>
+      <h1>Acho que vai</h1>
     </div>
   );
 }
