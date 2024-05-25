3b12eaf58dbe3087ce3e56ac046ea65e1abbca1d
jvmds <104597673+jvmds@users.noreply.github.com>
2024-05-25 19:20:40 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>Agora deu!</h1>
+      <h1>Testando o que o chatgpt falou</h1>
     </div>
   );
 }
