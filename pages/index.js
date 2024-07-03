2bb5532c364f651d5f4bc7246feeca6f44c464b8
jvmds <104597673+jvmds@users.noreply.github.com>
2024-07-03 19:38:45 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>TCC II</h1>
+      <h1>TCC II Teste</h1>
       <p>03/07/2024</p>
     </div>
   );
